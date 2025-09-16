bc2f3f4db5929b46027c53462fca4456a953482e
Alex Ros <alexrosmmii.ar@gmail.com>
2025-09-16 20:11:16 +0200

    Elimina la página de TestComponents y su navegación asociada en el componente Header. Se simplifica la estructura de rutas en la aplicación al remover referencias a componentes de prueba.

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import MainLayout from './Layouts/MainLayout';
 import Home from './pages/home-page';
 import Favorite from './pages/favorite-page';
 import Detail from './pages/detail-page';
-import TestComponentsPage from './pages/test-components-page';
 
 const App = () => {
   const routes = useRoutes([
@@ -14,7 +13,6 @@ const App = () => {
         { index: true, element: <Home /> },
         { path: 'favorite', element: <Favorite /> },
         { path: 'pokemon/:id', element: <Detail /> },
-        { path: 'test-components', element: <TestComponentsPage /> },
       ],
     },
   ]);
